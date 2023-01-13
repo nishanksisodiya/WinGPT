58537ec855dc76555bcd74f819ab8cba922f07b4
nishanksisodiya@gmail.com <nishanksisodiya@gmail.com>
2023-01-14 02:43:58 +0530

    Added Validators

---
+++ WinGPT.App/Validators/NotEmptyValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WinGPT.App.Validators;
+
+public class NotEmptyValidationRule : ValidationRule
+{
+    public string Message { get; set; }
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        if (string.IsNullOrWhiteSpace(value?.ToString()))
+        {
+            return new ValidationResult(false, Message ?? "A value is required");
+        }
+        return ValidationResult.ValidResult;
+    }
+}
